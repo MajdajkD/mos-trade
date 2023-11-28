9cdfdddc803529bd304f46fbb2bd89dd7ea85f6f
Daniel Majdajk <majdajkdaniel@msn.com>
2023-11-28 02:43:05 +0100

    initial

---
+++ interop/theme/theme.js
@@ -0,0 +1,17 @@
+﻿window.ThemeInterop = {
+  SwitchTheme: async function (isDark) {
+    if (isDark) {
+      document.body.setAttribute('data-bs-theme', 'dark');
+      localStorage.setItem('preferDark', 'true');
+    }
+    else {
+      document.body.setAttribute('data-bs-theme', '');
+      localStorage.setItem('preferDark', 'false');
+    }
+  },
+  IsDarkPreferred: async function () {
+    return localStorage.getItem('preferDark') == 'true';
+  }
+}
+
+window.ThemeInterop.SwitchTheme(localStorage.getItem('preferDark') == 'true');
---
+++ interop/tooltip/tooltip.js
@@ -0,0 +1,7 @@
+﻿window.TooltipInterop = {
+  SetTooltips: async function () {
+    window.tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    window.tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+  }
+}
+
